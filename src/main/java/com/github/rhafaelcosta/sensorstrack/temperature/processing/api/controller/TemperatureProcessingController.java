dcd2547550c9dc11a99272200a5de963b9a8c8ef
Rhafael Freitas da Costa <rhafael.costa@lactec.org.br>
2025-05-22 16:17:47 -0300

    Adicionado a funcionalidade de envio de eventos com RabbitMQ

--- src/main/java/com/github/rhafaelcosta/sensorstrack/temperature/processing/api/controller/TemperatureProcessingController.java
+++ src/main/java/com/github/rhafaelcosta/sensorstrack/temperature/processing/api/controller/TemperatureProcessingController.java
@@ -3,7 +3,10 @@ package com.github.rhafaelcosta.sensorstrack.temperature.processing.api.controll
 import com.github.rhafaelcosta.sensorstrack.temperature.processing.api.model.TemperatureLogResponse;
 import com.github.rhafaelcosta.sensorstrack.temperature.processing.common.IdGenerator;
 import io.hypersistence.tsid.TSID;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.MessagePostProcessor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -11,10 +14,15 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.time.OffsetDateTime;
 
+import static com.github.rhafaelcosta.sensorstrack.temperature.processing.infrastructure.rabbitmq.RabbitMQConfig.FANOUT_EXCHANGE_NAME;
+
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/sensors/{sensorId}/temperatures/data")
 public class TemperatureProcessingController {
+
+    private final RabbitTemplate rabbitTemplate;
 
     @PostMapping(consumes = MediaType.TEXT_PLAIN_VALUE)
     public void data(@PathVariable TSID sensorId, @RequestBody String input) {
@@ -39,6 +47,15 @@ public class TemperatureProcessingController {
                 .build();
 
         TemperatureProcessingController.log.info(log.toString());
+
+        Object payload = log;
+        String routingKey = "";
+        MessagePostProcessor messagePostProcessor = message -> {
+            message.getMessageProperties().setHeader("sensorId", log.getSensorId().toString());
+            return message;
+        };
+
+        rabbitTemplate.convertAndSend(FANOUT_EXCHANGE_NAME, routingKey, payload, messagePostProcessor);
     }
 
 }
